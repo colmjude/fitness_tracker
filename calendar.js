eca6882759918db6bff44a6fce9e06950a89b64c
Colm Britton <colmjude@gmail.com>
2014-01-29 11:07:09 +0000

    Improve readability of calendar.js

--- calendar.js
+++ calendar.js
@@ -1,14 +1,9 @@
 require('date-utils');
 var fut = require('forEachAsync');
 var fs = require('fs');
-var jsdom = require('jsdom');
-var jquery = require('jquery');
-var window = jsdom.jsdom(
-        '<html><head></head><body></body></html>').createWindow(); 
-// jquery-ize the window
-var jQuery = jquery(window);
 
-var weekdays_markup = ['<ol class="weekdays cf">\n',
+var weekdays = { "0": "Sunday", "1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday" },
+    weekdays_markup = ['<ol class="weekdays cf">\n',
 '    <li>S</li>',
 '    <li>M</li>',
 '    <li>T</li>',
@@ -19,39 +14,35 @@ var weekdays_markup = ['<ol class="weekdays cf">\n',
 '</ol>'].join('\n');
 
 var startdate = "January";
-var weekdays = { "0": "Sunday", "1": "Monday", "2": "Tuesday", "3": "Wednesday", "4": "Thursday", "5": "Friday", "6": "Saturday" };
 
-var now = new Date(),
-    current_month = now.getMonthName(),
-    current_year = now.getFullYear(),
-    month_num = Date.getMonthNumberFromName(current_month),
-    days_in_c_month = Date.getDaysInMonth(current_year, month_num);
+// console.log("year: ", current_year);
+// console.log("month #: ", month_num);
+// console.log("# of days in current month: ", days_in_c_month);
+// console.log("month started on a ", weekdays[day_of_first]);
+// console.log("finishes on a ", weekdays[last_of_month]);
 
-console.log("year: ", current_year);
-console.log("month #: ", month_num);
-console.log("# of days in current month: ", days_in_c_month);
+function generateCalendar() {
+    var now = new Date(),
+        current_month = now.getMonthName(),
+        current_year = now.getFullYear(),
+        month_num = Date.getMonthNumberFromName(current_month),
+        days_in_c_month = Date.getDaysInMonth(current_year, month_num);
+    var day_of_first = new Date(current_year, month_num, 1).getDay();
+    var last_of_month = new Date(current_year, month_num, days_in_c_month).getDay();
+    return generateMonthMarkup(current_month, current_year, month_num, days_in_c_month, day_of_first, last_of_month);
+}
 
-var day_of_first = new Date(current_year, month_num, 1).getDay();
-console.log("month started on a ", weekdays[day_of_first]);
-
-var last_of_month = new Date(current_year, month_num, days_in_c_month).getDay();
-console.log("finishes on a ", weekdays[last_of_month]);
-
-function generateMonthMarkup() {
+function generateMonthMarkup(month, year, mnum, no_days, firstday, lastday) {
     // probably need to sort this to handle a given date
-    var markup = '<h1>'+ current_month + ' ' + current_year +'</h1>\n';
+    var markup = '<h1>'+ month + ' ' + year +'</h1>\n';
     markup += weekdays_markup;
     markup += '<section class="calendar cf">\n';
-    markup += generateEndOfLastMonth(day_of_first, month_num, current_year);
-    markup += generateMonthList(days_in_c_month, current_month, current_year);
-    markup += generateStartOfNextMonth(last_of_month);
+    markup += generateEndOfLastMonth(firstday, mnum, year);
+    markup += generateMonthList(no_days, month, year);
+    markup += generateStartOfNextMonth(lastday);
     markup += '</section>\n';
     return markup;
 }
-
-// console.log(generateEndOfLastMonth(day_of_first));
-// console.log(generateMonthList(days_in_c_month, current_month, current_year));
-// console.log(generateStartOfNextMonth(last_of_month));
 
 function generateEndOfLastMonth(day_num, month_num, year) {
     var last_month;
@@ -100,14 +91,13 @@ function generateDateStr(num, name, year) {
 // markup array, join to get page
 var htmlQ = [];
 
-fut.forEachAsync(['partials/head.html', generateMonthMarkup, 'partials/footer.html'], function (next, element, index, array) {
+fut.forEachAsync(['partials/head.html', generateCalendar, 'partials/footer.html'], function (next, element, index, array) {
   if(isFunction(element)) {
       htmlQ.push( element() );
       next();
   } else {
       addPartialToQueue(element, htmlQ, next);
   }
-
 // then after all of the elements have been handled
 // the final callback fires to let you know it's all done
 }).then(function () {
