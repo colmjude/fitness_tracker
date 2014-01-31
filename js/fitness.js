b8af24110d043d2622946de2ce31ce1812a80977
Colm Britton <colmjude@gmail.com>
2014-01-31 17:32:06 +0000

    onload populate current month first then rest of current year months

--- js/fitness.js
+++ js/fitness.js
@@ -4,15 +4,26 @@
     var month = d.getMonthName();
     var activity_base_url = "http://runkeeper.com/user/colmjude/activity/";
     $.getJSON("data/fitness.json", function(data) {
-        var current_month = data[0][year][month];
-        for( var i = 0; i < current_month.length; i++) {
-            var $day = $('*[data-date="'+current_month[i]['date']+'"]', '.thismonth');
+        // populate current month first
+        populateMonth(data[0][year][month]);
+        // remove current month so isn't populated again
+        delete data[0][year][month];
+        // iterate through current year
+        for (var m in data[0][year]) {
+            populateMonth(data[0][year][m]);
+        }
+        //for(var i = 0; i < data[0][year].length)
+    });
+
+    function populateMonth(month_data) {
+        for( var i = 0; i < month_data.length; i++) {
+            var $day = $('*[data-date="'+month_data[i]['date']+'"]', '.thismonth');
             $day
                 .addClass("activity")
-                .addClass(current_month[i]['type']);
-            if(current_month[i]['activity_id']) {
-                $day.append( $("<a></a>", {"href": activity_base_url+current_month[i]['activity_id']}));
+                .addClass(month_data[i]['type']);
+            if(month_data[i]['activity_id']) {
+                $day.append( $("<a></a>", {"href": activity_base_url+month_data[i]['activity_id']}));
             }
         }
-    });
+    }
 }(jQuery));
