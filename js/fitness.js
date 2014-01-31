822dd33c7070e7c9ee2823ebd8300bd38633e6b9
Colm Britton <colmjude@gmail.com>
2014-01-31 17:01:35 +0000

    link runs to runkeeper activities

--- js/fitness.js
+++ js/fitness.js
@@ -2,12 +2,17 @@
     var d = new Date();
     var year = d.getFullYear();
     var month = d.getMonthName();
+    var activity_base_url = "http://runkeeper.com/user/colmjude/activity/";
     $.getJSON("data/fitness.json", function(data) {
         var current_month = data[0][year][month];
         for( var i = 0; i < current_month.length; i++) {
-            $('*[data-date="'+current_month[i]['date']+'"]', '.thismonth')
+            var $day = $('*[data-date="'+current_month[i]['date']+'"]', '.thismonth');
+            $day
                 .addClass("activity")
                 .addClass(current_month[i]['type']);
+            if(current_month[i]['activity_id']) {
+                $day.append( $("<a></a>", {"href": activity_base_url+current_month[i]['activity_id']}));
+            }
         }
     });
 }(jQuery));
